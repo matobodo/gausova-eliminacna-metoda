3d25807a15faf5607d4ae1701981915276850021
matobodo <bodicky.martin@gmail.com>
2016-09-22 18:32:03 +0200

    zlomky

--- main.c
+++ main.c
@@ -1,63 +1,116 @@
 #include <stdio.h>
 
+int max(int a, int b)
+{
+    return a > b ? a : b;
+}
+
+int min(int a, int b)
+{
+    return a < b ? a : b;
+}
+
+int gcd(int _a, int _b)
+{
+    _a < 0 ? _a = -_a :0;
+    _b < 0 ? _b = -_b :0;
+    int a = max(_a, _b);
+    int b = min(_a, _b);
+    int x;
+    b != 0 ? (x = a - (a / b) * b) : (x = -1);
+    switch (x) {
+    case -1:
+        return 1;
+        break;
+    case 0:
+        return b;
+        break;
+    default:
+        return gcd(b, x);
+        break;
+    }
+}
+
 int main()
 {//while(0 == 0){
-    //FILE *f = fopen("C:/Programovanie/build-gausova-eliminacna-metoda-Desktop_Qt_5_7_0_MinGW_32bit-Debug/vstup.txt", "r");
-    printf("Zadaj pocet rovnic: ");
+    FILE *f = fopen("C:/Programovanie/build-gausova-eliminacna-metoda-Desktop_Qt_5_7_0_MinGW_32bit-Debug/vstup.txt", "r");
+    //printf("Zadaj pocet rovnic: ");
     int rovnic;
-    scanf("%i", &rovnic);
-    //fscanf(f, "%i", &rovnic);
-    printf("Zadaj pocet premennych: ");
+    //scanf("%d", &rovnic);
+    fscanf(f, "%d", &rovnic);
+    //printf("Zadaj pocet premennych: ");
     int premennych;
-    scanf("%i", &premennych);
-    //fscanf(f, "%i", &premennych);
+    //scanf("%d", &premennych);
+    fscanf(f, "%d", &premennych);
 
     float matica[rovnic][premennych + 1];
-    printf("Zadaj maticu:\n");
+    float citatel[rovnic][premennych + 1];
+    float menovatel[rovnic][premennych + 1];
+    //printf("Zadaj maticu:\n");
     for (int r = 0; r < rovnic; ++r) {
         for (int p = 0; p < premennych + 1; ++p) {
-            scanf("%f", &matica[r][p]);
-            //fscanf(f, "%f", &matica[r][p]);
+            //scanf("%f", &matica[r][p]);
+            fscanf(f, "%f", &matica[r][p]);
+            citatel[r][p] = matica[r][p];
+            menovatel[r][p] = 1;
         }
     }
 
 
     int stlpec = 0;
     for (int r = 0; r < rovnic; ++r) { // na trojuholnik
-        float delitel = matica[r][stlpec]; // prvok na diagonale
-        if (delitel != 1.0F && delitel != 0.0F) { // ak je rovny jednej alebo nulovy nema zmysel delit
+        float nasobokC = citatel[r][stlpec]; // prvok na diagonale
+        float nasobokM = menovatel[r][stlpec]; // prvok na diagonale
+        if (nasobokC != 1.0F && nasobokC != 0.0F) { // ak je rovny jednej alebo nulovy nema zmysel delit
             for (int p = stlpec; p < premennych + 1; ++p) { // ziskanie pivotnej jednotky a vydelenie ostatnych prvkov
-                matica[r][p] /= delitel;
+                citatel[r][p] *= nasobokM;
+                menovatel[r][p] *= nasobokC;
             }
-        } else if (delitel == 0.0F) { //ak je nulovy prvok na diagonale tak vymen riadky aby bol nenulovy
+        } else if (nasobokC == 0.0F) { //ak je nulovy prvok na diagonale tak vymen riadky aby bol nenulovy
             for (int R = r + 1; R < rovnic; ++R) {
-                if (matica[R][stlpec] != 0.0F) { // ak som na nenulovom prvku tak vymenim riadky
+                if (citatel[R][stlpec] != 0.0F) { // ak som na nenulovom prvku tak vymenim riadky
                     for (int P = stlpec; P < premennych + 1; ++P) {
-                        float m = matica[r][P];
-                        matica[r][P] = matica[R][P];
-                        matica[R][P] = m;
+                        float c = citatel[r][P];
+                        float m = menovatel[r][P];
+                        citatel[r][P] = citatel[R][P];
+                        menovatel[r][P] = menovatel[R][P];
+                        citatel[R][P] = c;
+                        menovatel[R][P] = m;
                     }
                     R = rovnic; // ukoncenie hladania po vymene
                 }
             }
         }
-        if (matica[r][stlpec] == 1.0F) { // nulujem len ak mam pivotnu jednotku
+        if (citatel[r][stlpec] == menovatel[r][stlpec]) { // nulujem len ak mam pivotnu jednotku
             for (int R = r + 1; R < rovnic; ++R) { // vynulovanie stlpca
-                float nasobok = matica[R][stlpec]; // vzdy o riadok nizsie rovnaka premenna
-                if (nasobok != 0.0F) { // ak je nulovy nema zmysel odpocitavat
+                float nasobokC = citatel[R][stlpec]; // vzdy o riadok nizsie rovnaka premenna
+                float nasobokM = menovatel[R][stlpec]; // vzdy o riadok nizsie rovnaka premenna
+                if (nasobokC != 0.0F) { // ak je nulovy nema zmysel odpocitavat
                     for (int P = stlpec; P < premennych + 1; ++P) {
-                        matica[R][P] -= nasobok*matica[r][P];
+                        float citatelR = nasobokC*citatel[r][P];
+                        float menovatelR = nasobokM*menovatel[r][P];
+                        citatel[R][P] = citatel[R][P]*menovatelR - menovatel[R][P]*citatelR;
+                        menovatel[R][P] *= menovatelR;
+                        //int D = gcd(citatel[R][P], menovatel[R][P]); // najvacsi spolocny delitel
+                        //citatel[R][P] /= D;
+                        //menovatel[R][P] /= D;
                     }
                 }
             }
         }
-        if (matica[r][stlpec] == 1.0F && stlpec + 1 < premennych) { // presun na dalsi stlpec len ak som dostal pivotnu jednotk
+        if (citatel[r][stlpec] == menovatel[r][stlpec] && stlpec + 1 < premennych) { // presun na dalsi stlpec len ak som dostal pivotnu jednotk
             stlpec++;
-        } else if (matica[r][stlpec] != 0.0F && matica[r][stlpec] != 1.0F) { // ak nemam pivotnu jednotku tak ju ziskaj
+        } else if (citatel[r][stlpec] != 0.0F && citatel[r][stlpec] != menovatel[r][stlpec]) { // ak nemam pivotnu jednotku tak ju ziskaj
             r--;
-        } else if (matica[r][stlpec] == 0.0F) {
+        } else if (citatel[r][stlpec] == 0.0F) {
             stlpec++;
             r--;
+        }
+    }
+
+    for (int r = 0; r < rovnic; ++r) {
+        for (int p = 0; p < premennych; ++p) {
+            matica[r][p] = citatel[r][p] / menovatel[r][p];
         }
     }
 
