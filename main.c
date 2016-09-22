0c47b1f50dfa13e78f511cfbeda9525c6978ec39
matobodo <bodicky.martin@gmail.com>
2016-09-22 02:19:37 +0200

    uprava pre viac rovnic ako premennych

--- main.c
+++ main.c
@@ -17,33 +17,46 @@ int main()
         }
     }
 
+
+    int stlpec = 0;
     for (int r = 0; r < rovnic; ++r) { // na trojuholnik
-        float delitel = matica[r][r]; // prvok na diagonale
+        bool dalsiStlpec = false;
+        float delitel = matica[r][stlpec]; // prvok na diagonale
         if (delitel != 1.0F && delitel != 0.0F) { // ak je rovny jednej alebo nulovy nema zmysel delit
-            for (int p = r; p < premennych + 1; ++p) { // ziskanie pivotnej jednotky a vydelenie ostatnych prvkov
+            for (int p = stlpec; p < premennych + 1; ++p) { // ziskanie pivotnej jednotky a vydelenie ostatnych prvkov
                 matica[r][p] /= delitel;
             }
         }
         for (int R = r + 1; R < rovnic; ++R) { // vynulovanie stlpca
-            float nasobok = matica[R][r]; // vzdy o riadok nizsie rovnaka premenna
+            float nasobok = matica[R][stlpec]; // vzdy o riadok nizsie rovnaka premenna
             if (nasobok != 0.0F) { // ak je nulovy nema zmysel odpocitavat
-                for (int P = r; P < premennych + 1; ++P) {
+                for (int P = stlpec; P < premennych + 1; ++P) {
                     matica[R][P] -= nasobok*matica[r][P];
                 }
+                dalsiStlpec = true;
             }
+        }
+        if (dalsiStlpec && stlpec + 1 < premennych) { // presun na dalsi stlpec len ak som dostal pivotnu jednotku
+            stlpec++;
         }
     }
 
+    stlpec = premennych - 1;
     for (int r = rovnic - 1; r >= 0; --r) { // nulovanie
+        bool dalsiStlpec = false;
         for (int R = r - 1; R >= 0; --R) {
-            if (matica[r][r] != 0.0F) { // ak je pivotny prvok nulovy, vynecham ho
-                float nasobok = matica[R][r];
-                if (nasobok == 0.0F) { // ak je nulovy nema zmysel odpocitavat
-                    for (int P = r; P < premennych + 1; ++P) { // vynulovanie stlpca
+            if (matica[r][stlpec] != 0.0F) { // ak je pivotny prvok nulovy, vynecham ho
+                float nasobok = matica[R][stlpec]; // o riadok vyssie rovnaka premenna
+                if (nasobok != 0.0F) { // ak je nulovy nema zmysel odpocitavat
+                    for (int P = stlpec; P < premennych + 1; ++P) { // vynulovanie stlpca
                         matica[R][P] -= nasobok*matica[r][P];
                     }
+                    dalsiStlpec = true;
                 }
             }
+        }
+        if (dalsiStlpec && stlpec - 1 >= 0) { // presun na dalsi stlpec len ak je pivotny prvok nenulovy
+            stlpec--;
         }
     }
 
